c7cbe43c4975692a05e87b511ae43184660961e9
Jonathan Chavez <56609911+djohnnychavez@users.noreply.github.com>
2019-12-20 15:40:17 -0600

    Add files via upload
    The purpose of this code was to have one application that will launch several applications at once.
    I followed a tutorial to create this one.

---
+++ appproject.py
@@ -0,0 +1,61 @@
+#the purpose of this project is to have an application that will open several other apps at once
+
+
+import tkinter as tk
+from tkinter import filedialog, Text
+import os
+
+root = tk.Tk()
+apps = []
+
+if os.path.isfile("save.txt"):
+    with open("save.txt", "r") as f:
+        tempApps = f.read()
+        tempApps = tempApps.split(",")
+        apps = [x for x in tempApps if x.strip()]
+        
+
+
+def addApp():
+
+    for widget in frame.winfo_children():
+        widget.destroy()
+
+
+    filename = filedialog.askopenfilename(initialdir="/", title="Select File",
+    filetypes=(("executables","*.exe"), ("all files", "*.*")))
+    apps.append(filename)
+    print(filename)
+    for app in apps:
+        label = tk.Label(frame, text=app, bg="gray")
+        label.pack()
+
+def runApps():
+    for app in apps:
+        os.startfile(app)
+
+
+canvas = tk.Canvas(root, height=500, width=500, bg="#263D42")
+canvas.pack()
+
+#this is creating a white frame inside of the GUI
+frame = tk.Frame(root, bg="white")
+frame.place(relwidth=0.7, relheight=0.7, relx=0.1, rely=0.1)
+
+#this is to create a button called openFile
+openFile = tk.Button(root, text="Open File", padx =10, pady=5, fg="white", bg="#263D42", command=addApp)
+openFile.pack()
+
+runApps = tk.Button(root, text="runApps", padx =10, pady=5, fg="white", bg="#263D42", command=runApps)
+runApps.pack()
+
+
+for app in apps:
+    label = tk.Label(frame, text=app)
+    label.pack()
+
+root.mainloop()
+
+with open("save.txt", "w") as f:
+    for app in apps:
+        f.write(app + ",")
